eb3963e7dc30b762392d303c29f7185a14befeff
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-02-11 16:15:06 +0900

    first commit for angular template

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mixideaWebApp
+ * @description
+ * # mixideaWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mixideaWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
