c2d58f618e5fedde68400efaf7c0ff60616ece4d
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-04-11 02:11:16 +0900

    header apply icons and login menu design changed

--- app/scripts/controllers/header-user.js
+++ app/scripts/controllers/header-user.js
@@ -8,13 +8,82 @@
  * Controller of the mixideaWebApp
  */
 angular.module('mixideaWebApp')
-  .controller('HeaderUserCtrl',['$scope','UserAuthService','$uibModal' , function ($scope, UserAuthService, $uibModal) {
+  .controller('HeaderUserCtrl',['$scope','UserAuthService','$uibModal' ,'$state', function ($scope, UserAuthService, $uibModal, $state) {
 
   	$scope.user = UserAuthService;
+  	$scope.header_below_type = "menu";
+  	$scope.show_header_below = false;
+  	$scope.menu_list = new Array();
 
   	$scope.logout = function(){
 		$scope.user.logout();
+		console.log("logout");
+		$scope.show_header_below = false;
   	}
+  	
+  	$scope.menu_click = function(param){
+  		console.log("menu_click" + param);
+  		switch(param){
+  			case "link_eventlist":
+  				$scope.link_eventlist();
+  			break;
+  			case "link_articlelist":
+  				$scope.link_articlelist();
+  			break;
+  			case "link_mypage":
+  				$scope.link_mypage();
+  			break;
+  			case "logout":
+  				$scope.logout();
+  			break;
+  		}
+  	}
+
+
+  	$scope.link_articlelist = function(){
+		console.log("link_articlelist");
+		$scope.show_header_below = false;
+
+  	}
+  	$scope.link_eventlist = function(){
+		console.log("link_eventlist");
+  		$state.go('eventsearch_layout_three_column.list');
+		$scope.show_header_below = false;
+  	}
+  	$scope.link_mypage = function(){
+		console.log("link_mypage");
+		$scope.show_header_below = false;
+  		
+  	}
+
+  	$scope.click_notification = function(){
+		console.log("click_notification");
+  		
+  	}
+  	$scope.click_message = function(){
+		console.log("click_message");
+  		
+  	}
+  	$scope.click_hamburger = function(){
+		console.log("click_hamburger");
+		$scope.show_header_below = !$scope.show_header_below;
+  		$scope.header_below_type = "menu";
+  		var window_width = document.documentElement.clientWidth;
+  		if(window_width < 640){
+  			$scope.menu_list = [
+  			{name:"Event List", func_param:"link_eventlist"},
+  			{name:"Article List", func_param:"link_articlelist"},
+  			{name:"My Page", func_param:"link_mypage"},
+  			{name:"Logout", func_param:"logout"},
+  			];
+  		}else{
+  			$scope.menu_list = [
+  			{name:"Logout", func_param:"logout"}
+  			];
+  		}
+
+  	}
+
 
 
 /*
